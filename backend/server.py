40ce9786f7fea0879117a42122482d137f10cf08
topkitfr <topkitfr@gmail.com>
2026-03-30 09:55:03 +0200

    modified:   backend/server.py

--- backend/server.py
+++ backend/server.py
@@ -8,7 +8,8 @@ import logging
 import time
 from collections import defaultdict
 
-from .database import db, client
+from database import db, client
+
 
 from .routers.auth import router as auth_router
 from .routers.kits import router as kits_router
